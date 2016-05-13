cd68ca6df4f8151ab1a7810b0eb995c22b1bb7d0
Riccardo Cirimelli <rcirimelli@gmail.com>
2016-05-13 16:03:16 +0200

    Added test/example for entire run

---
+++ tests/test_processr.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from processr.processr import process
+
+
+def test_rename():
+    provided_input = {'the_answer': 42}
+    expected_output = {'the_answer_to_the_ultimate_question': 42}
+    pipeline = [
+        ('rename_keys', {'the_answer': 'the_answer_to_the_ultimate_question'})
+    ]
+
+    output = process(provided_input, pipeline)
+    assert output == expected_output
